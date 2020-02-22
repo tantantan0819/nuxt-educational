40b586e37d856d61586b9ea57cc5e0024deb5549
tanhongyu <3266415926@qq.com>
2020-02-22 16:15:49 +0800

    修改

--- src/pages/letter/index.vue
+++ src/pages/letter/index.vue
@@ -19,7 +19,7 @@
           <el-table-column prop="relation" label="与您的关系" width="100" :formatter="relashipFormat"></el-table-column>
           <el-table-column prop="email" label="单位邮箱" width="120"></el-table-column>
           <el-table-column prop="createtime" label="创建时间" ></el-table-column>
-          <el-table-column prop="operation" label="操作" width="80">
+          <el-table-column prop="operation" label="操作" width="100">
             <template slot-scope="scope">
               <el-button size="mini" @click="handleEdit(scope.$index, scope.row)" class="viewBtn">查看详情</el-button>
             </template>
