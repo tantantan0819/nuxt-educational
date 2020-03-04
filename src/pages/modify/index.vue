72c839792eaf2bfd65c80990054dcd995308f67a
tanhongyu <3266415926@qq.com>
2020-03-04 13:23:45 +0800

    过滤材料类型

--- src/pages/modify/index.vue
+++ src/pages/modify/index.vue
@@ -8,7 +8,7 @@
       </div>
       <div class="cv_content cv2 centerTable">
         <el-table :data="tableData" stripe style="width: 100%">
-          <el-table-column prop="page_type" label="材料类型"></el-table-column>
+          <el-table-column prop="page_type" label="材料类型" :formatter="noteFormat"></el-table-column>
           <el-table-column prop="note_type" label="修改类别" :formatter="typeFormat"></el-table-column>
           <el-table-column prop="note_body" label="备注"></el-table-column>
           <el-table-column prop="create_time" label="提交时间" :formatter="timeFormat"></el-table-column>
@@ -62,15 +62,14 @@
               <el-form-item label="附件:" prop="files">
                 <div class="note_item" v-for="(item,index) in modify.note_files" :key="index">
                   <span>
-                    <i class="el-icon-document"></i>{{item.name}}
+                    <i class="el-icon-document"></i>
+                    {{item.name}}
                   </span>
                   <span class="note_view el-icon-view" @click="view(item.url)"></span>
                 </div>
-              </el-form-item > 
-                <el-form-item label="提交时间:" class="modify_itme">
-               <span>
-                    {{modify.create_time | timeFormat2}}
-                  </span>
+              </el-form-item>
+              <el-form-item label="提交时间:" class="modify_itme">
+                <span>{{modify.create_time | timeFormat2}}</span>
               </el-form-item>
             </el-form>
             <div class="footer_button">
@@ -90,7 +89,7 @@
 <script>
 import http from "~/plugins/http";
 import config from "~/plugins/config";
-import { getStore, setStore,timeDetail } from "~/plugins/utils";
+import { getStore, setStore, timeDetail } from "~/plugins/utils";
 let that;
 export default {
   layout: "utrack",
@@ -109,7 +108,7 @@ export default {
       }
     };
   },
-  beforeCreate(){
+  beforeCreate() {
     that = this;
   },
   mounted() {
@@ -133,19 +132,19 @@ export default {
       return flag;
     }
   },
-  filters:{
+  filters: {
     //显示修改类型
-    typeFormat2(type){
+    typeFormat2(type) {
       let flag = true;
-      that.modify.note_type == type ? flag = false : flag = true;
-      return flag;  
+      that.modify.note_type == type ? (flag = false) : (flag = true);
+      return flag;
     },
     //显示提交时间
-     //过滤关系
+    //过滤关系
     timeFormat2(val) {
-      let time = timeDetail(val)
+      let time = timeDetail(val);
       return time;
-    },
+    }
   },
   methods: {
     //获取类别下拉
@@ -155,18 +154,18 @@ export default {
       if (noteType) {
         _this.select.noteType = noteType.NOTE_TYPE_CV;
         _this.modify.type = noteType.NOTE_TYPE_CV[0].id;
-      //获取修改信息
-      http.post("/customer-note/list").then(res => {
+        //获取修改信息
+        http.post("/customer-note/list").then(res => {
           _this.tableData = res.rows;
-      });
+        });
       } else {
         http.get("/code-val/group-key-list").then(res => {
           _this.select.noteType = res.NOTE_TYPE_CV;
           _this.modify.type = res.NOTE_TYPE_CV[0].id;
-             //获取修改信息
-            http.post("/customer-note/list").then(res => {
-                _this.tableData = res.rows;
-            });
+          //获取修改信息
+          http.post("/customer-note/list").then(res => {
+            _this.tableData = res.rows;
+          });
           setStore("noteType", res);
         });
       }
@@ -175,16 +174,35 @@ export default {
     //过滤性别
     typeFormat(row, column) {
       let type = "";
-       if(this.select.noteType.length>0){
-         this.select.noteType.map(item=>{
-           item.id == row.note_type ? type = item.cvalue_cn : '';
-         })
-       }
+      if (this.select.noteType.length > 0) {
+        this.select.noteType.map(item => {
+          item.id == row.note_type ? (type = item.cvalue_cn) : "";
+        });
+      }
       return type;
+    },
+    //过滤材料类型
+    noteFormat(row) {
+      let relust = "cv";
+      switch (row.page_type) {
+        case "cv":
+          relust = "CV";
+          break;
+        case "ps":
+          relust = "PS";
+          break;
+        case "res":
+          relust = "推荐信";
+          break;
+        default:
+          relust = "CV";
+          break;
+      }
+      return relust;
     },
     //过滤关系
     timeFormat(row, column) {
-      let time = timeDetail(row.create_time)
+      let time = timeDetail(row.create_time);
       return time;
     },
     //取消查看详情
@@ -193,19 +211,19 @@ export default {
       _this.isDetail = false;
     },
     //查看详情
-    handleEdit(index,row) {
+    handleEdit(index, row) {
       let _this = this;
       _this.modify = row;
       _this.isDetail = true;
-      _this.modify.note_files = JSON.parse(_this.modify.note_files)
+      _this.modify.note_files = JSON.parse(_this.modify.note_files);
     },
     // 关闭查看详情
     close() {
       this.psDetail = false;
     },
     //查看附件
-    view(url){
-      window.open(config.view_host+url,'_blank')
+    view(url) {
+      window.open(config.view_host + url, "_blank");
     }
   }
 };
