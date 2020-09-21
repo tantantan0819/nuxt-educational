e4b227df40571ec60e5413e4834482a8e70ad5e2
tanghongyu <3266415926@qq.com>
2020-09-21 16:16:01 +0800

    去掉头部默认值

--- src/components/rHeader.vue
+++ src/components/rHeader.vue
@@ -69,19 +69,17 @@ export default {
             uid: '',
             person:{},//sso获取到的用户信息
             user: {
-                avatar:
-                    "https://ss0.bdstatic.com/70cFuHSh_Q1YnxGkpoWK1HF6hhy/it/u=2205650502,1425754204&fm=26&gp=0.jpg",
-                // avatar: '/images/refactor/adavar.png',
-                name: "小明",
-                email: "1111111@qq.com",
-                tel: "18381001111",
-                school: "清华大学",
-                major: "计算机科学与技术",
-                center: "西安分中心",
-                counselor: "张老师", //顾问老师
-                apply: "李老师", //申请老师
-                writ: "杨老师", //文书老师
-                counselor_tel: "18381007777",
+                avatar: "",
+                name: "",
+                email: "",
+                tel: "",
+                school: "",
+                major: "",
+                center: "",
+                counselor: "", //顾问老师
+                apply: "", //申请老师
+                writ: "", //文书老师
+                counselor_tel: "",
                 teacher: '',//教育顾问
                 teacher_tel: '',//教育顾问电话
                 entrance:'',//入学顾问
